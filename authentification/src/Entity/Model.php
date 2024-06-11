1d3c7441c6ca09f767308e2761da3a2aa6a7dd86
kholoud001 <kholoud.sanak@gmail.com>
2024-06-11 12:53:38 +0100

    model entity

---
+++ authentification/src/Entity/Model.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Entity;
+
+use App\Repository\ModelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ModelRepository::class)]
+class Model
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(targetEntity: Mark::class)]
+    private ?Mark $mark = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMark(): ?Mark
+    {
+        return $this->mark;
+    }
+
+    public function setMark(?Mark $mark): self
+    {
+        $this->mark = $mark;
+
+        return $this;
+    }
+}
