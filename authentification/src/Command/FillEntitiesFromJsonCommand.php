4b170c4a07ab5eab79074366e163e2ce08b9ca5f
kholoud001 <kholoud.sanak@gmail.com>
2024-06-11 12:53:22 +0100

    json cmd to fill entites

---
+++ authentification/src/Command/FillEntitiesFromJsonCommand.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Command;
+
+use App\Entity\Mark;
+use App\Entity\Model;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FillEntitiesFromJsonCommand extends Command
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('fill:entities') // Set command name here
+            ->setDescription('Fill Mark and Model entities from JSON file');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $jsonFilePath = $this->getApplication()->getKernel()->getProjectDir() . '/public/dataset_model_mark.json';
+
+        $jsonData = file_get_contents($jsonFilePath);
+        if ($jsonData === false) {
+            $output->writeln('<error>Failed to read JSON file.</error>');
+            return Command::FAILURE;
+        }
+
+        $data = json_decode($jsonData, true);
+        if ($data === null) {
+            $output->writeln('<error>Failed to decode JSON data.</error>');
+            return Command::FAILURE;
+        }
+
+        foreach ($data as $item) {
+            $markName = $item['mark'];
+            $modelName = $item['model'];
+
+            // Find or create Mark entity
+            $mark = $this->entityManager->getRepository(Mark::class)->findOneBy(['name' => $markName]);
+            if (!$mark) {
+                $mark = new Mark();
+                $mark->setName($markName);
+                $this->entityManager->persist($mark);
+            }
+
+            // Create Model entity and associate it with Mark
+            $model = new Model();
+            $model->setName($modelName);
+            $model->setMark($mark);
+            $this->entityManager->persist($model);
+        }
+
+        $this->entityManager->flush();
+
+        $output->writeln('Entities filled successfully.');
+
+        return Command::SUCCESS;
+    }
+
+}
