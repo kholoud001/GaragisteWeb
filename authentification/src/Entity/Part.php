78a1d3f739f1c08c28949572647a12086b117fad
kholoud001 <kholoud.sanak@gmail.com>
2024-06-12 11:47:15 +0100

    part n assoc n report

--- authentification/src/Entity/Part.php
+++ authentification/src/Entity/Part.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PartRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PartRepository::class)]
@@ -16,6 +18,15 @@ class Part
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    // Add the collection of reports
+    #[ORM\ManyToMany(targetEntity: Report::class, mappedBy: "parts")]
+    private Collection $reports;
+
+    public function __construct()
+    {
+        $this->reports = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -26,10 +37,29 @@ class Part
         return $this->name;
     }
 
-    public function setName(string $name): static
+    public function setName(string $name): self
     {
         $this->name = $name;
+        return $this;
+    }
 
+    // Getters and setters for reports
+    public function getReports(): Collection
+    {
+        return $this->reports;
+    }
+
+    public function addReport(Report $report): self
+    {
+        if (!$this->reports->contains($report)) {
+            $this->reports[] = $report;
+        }
+        return $this;
+    }
+
+    public function removeReport(Report $report): self
+    {
+        $this->reports->removeElement($report);
         return $this;
     }
 }
