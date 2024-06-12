f0135c9fc09529378d670e4f3df1069be5deeaa8
kholoud001 <kholoud.sanak@gmail.com>
2024-06-12 11:46:58 +0100

    model association n to report

--- authentification/src/Entity/Model.php
+++ authentification/src/Entity/Model.php
@@ -2,6 +2,8 @@
 namespace App\Entity;
 
 use App\Repository\ModelRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ModelRepository::class)]
@@ -17,6 +19,37 @@ class Model
 
     #[ORM\ManyToOne(targetEntity: Mark::class)]
     private ?Mark $mark = null;
+
+    // Add this property to hold the collection of reports
+    #[ORM\ManyToMany(targetEntity: Report::class, mappedBy: "models")]
+    private Collection $reports;
+
+    public function __construct()
+    {
+        $this->reports = new ArrayCollection();
+    }
+
+    // Add methods to manage the collection
+    public function getReports(): Collection
+    {
+        return $this->reports;
+    }
+
+    public function addReport(Report $report): self
+    {
+        if (!$this->reports->contains($report)) {
+            $this->reports[] = $report;
+        }
+
+        return $this;
+    }
+
+    public function removeReport(Report $report): self
+    {
+        $this->reports->removeElement($report);
+
+        return $this;
+    }
 
     public function getId(): ?int
     {
