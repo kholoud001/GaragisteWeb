a757809dc622ab4bd53a4184bd64fc06d203e7bb
kholoud001 <kholoud.sanak@gmail.com>
2024-06-12 14:18:54 +0100

    add report to db controller

--- authentification/src/Controller/ReportController.php
+++ authentification/src/Controller/ReportController.php
@@ -2,16 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Report;
+use App\Form\ReportFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ReportController extends AbstractController
 {
     #[Route('/reports', name: 'app_report')]
     public function index(): Response
     {
-        return $this->render('reports/index.html.twig');
+        return $this->render('report/index.html.twig');
 
     }
+
+     /**
+     * @Route("/report/new", name="report_new", methods={"GET", "POST"})
+     */
+     /**
+     * @Route("/report/new", name="report_new")
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $report = new Report();
+        $form = $this->createForm(ReportFormType::class, $report);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($report);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('report_new');
+        }
+
+        return $this->render('report/report_file.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
